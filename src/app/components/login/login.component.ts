6630927312e68ba1238be3a85ea9ee963d229d37
NikitaGrimes <nikita.rick@gmail.com>
2023-06-27 13:10:26 +0300

    login view

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public hide = true;
+  public loginForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder){
 
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: this.formBuilder.control('', [Validators.required]),
+      password: this.formBuilder.control('', [Validators.required])
+    });
+  }
+
+  public getUsernameErrorMessage(): string{
+    return "Enter your username.";
+  }
+
+  public getPasswordErrorMessage(): string{
+    return "Enter your password."
+  }
+
+  public submit(): void{
+    const username = this.loginForm.controls["username"].value;
+    const password = this.loginForm.controls["password"].value;
+    
+  }
 }
--- src/app/modules/auth.module.ts
+++ src/app/modules/auth.module.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../components/login/login.component';
 import { TodosComponent } from '../components/todos/todos.component';
 import { AuthRoutingModule } from '../rooters/auth-routing.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -13,7 +17,13 @@ import { AuthRoutingModule } from '../rooters/auth-routing.module';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule, 
+    MatIconModule
   ]
 })
 export class AuthModule { }
