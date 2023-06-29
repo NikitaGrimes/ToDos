ea68b43ff36acbfca6f3a7fbbb9a52bb7740c40d
NikitaGrimes <nikita.rick@gmail.com>
2023-06-29 11:58:36 +0300

    remove deadcode

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/routers/app.routing';
@@ -10,9 +10,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    HttpClient,
-    BrowserModule,
-    HttpClientModule,
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
